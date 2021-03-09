aaf3f1dc58474f8c6a07b85405488c722477538a
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-03-09 16:06:24 -0400

    validação sem perdar de dados nos campos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Http\requests\StoreUpdatePost;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,7 @@ class PostController extends Controller
     public function create(){
        return view('admin.posts.create');
     }
-    public function store(Request $request){
+    public function store(StoreUpdatePost $request){
         $post = Post::create($request->all());
         return redirect()->route('posts.index');
     }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,16 @@
+<h1>Cadastrar Post</h1>
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach   
+    </ul>
+@endif
 <form action="{{route('posts.store')}}" method="post">
     @csrf
-    <input type="text" name="title" id="title"><br>
-    <textarea type="text" name="content" id="content" cols="30" rows="4"></textarea><br>
+    <input type="text" name="title" id="title" value="{{old('title')}}"><br>
+    <textarea type="text" name="content" id="content" cols="30" rows="4">{{old('content')}}</textarea><br>
     <button type="submit">Enviar</button>
 
 
